fecd5b652937f20b5305d4046ed1c7db70027207
Charles <zhaozhuoran@modjor.com>
2025-03-16 07:54:05 +0800

    build

--- svelte.config.js
+++ svelte.config.js
@@ -6,13 +6,16 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html',
+			fallback: 'index.html',
 			precompress: false,
 			strict: true
 		}),
 		files: {
 			routes: 'src/routes',
 			serviceWorker: 'src/service-worker.js'
+		},
+		paths: {
+			base: ''
 		}
 	}
 };
